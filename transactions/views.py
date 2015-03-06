62297da9f8cc0799e2f182615837d3a36beb6ce5
goexpensive <goexpensive@gmail.com>
2015-03-05 21:23:35 -0300

    Cambio de gitignore

--- transactions/views.py
+++ transactions/views.py
@@ -3,6 +3,7 @@ from django.views.generic import View
 from django.shortcuts import render
 from transactions.models import Transaction
 from mercadopago.mercadopago import *
+import json
 
 
 class TransactionView(View):
@@ -10,11 +11,22 @@ class TransactionView(View):
 	template_name = 'lista_de_pruebas.html'
 
 	def get(self, request, **kwargs):
-	    mp = MP("client_id", "client_secret")
+	    mp = MP("2393090223886468", "eXxJNpUGgoxbmD84QJbvmNVuI2TKlNQ9")
 	    paymentInfo = mp.get_payment_info (request.GET["id"])
 	    
 	    if paymentInfo["status"] == 200:
-	    	return render(request, self.template_name, {'Status': paymentInfo['status'],'Response': paymentInfo['response']})
+	    	json_data = json.dumps(paymentInfo)
+	    	HttpResponse(json_data,content_type='application/json')
+	    	return HttpResponse(json_data,content_type='application/json')
 	    else:
-	        return render(request, self.template_name, {'Status': paymentInfo['status'],'Response': paymentInfo['response']})
+	        paymentInfo['status'] = self.post(request)
+	        json_data = json.dumps(paymentInfo)
+	        return HttpResponse(json_data,content_type='application/json')
+	#Guardar los datos en el modelo.     
+	def post(self, request, **kwargs):
+			paymentInfo = 'Es un post'
+			return paymentInfo
+	#Obtener los datos necesarios de la transacción.
+	def get_resource_data()
+		pass
 
